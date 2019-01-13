0df29934565473cc03fb67368f4628e9398fbb07
Alejandro <atabord@hotmail.com>
2019-01-12 20:26:56 -0500

    adding timer

--- js/app.js
+++ js/app.js
@@ -11,6 +11,19 @@ $(function () {
       color: '#DCFF0E',
     }, 1000, colorWhite)
   }
+  
+  function initTime() {
+    var timer = new easytimer.Timer();
+    timer.start({countdown: true, startValues: {minutes: 2}});
+    $('#timer').html(timer.getTimeValues().toString().slice(3));
+    timer.addEventListener('secondsUpdated', function (e) {
+        $('#timer').html(timer.getTimeValues().toString().slice(3));
+    });
+    // timer.addEventListener('targetAchieved', function (e) {
+    //     $('#countdownExample .values').html('KABOOM!!');
+    // });
+  }
 
   colorWhite();
+  initTime();
 })
